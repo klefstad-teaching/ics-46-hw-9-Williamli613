0b954598e9a0eb9f0c21880eff4d3ddbd0869a91
Williamli613 <jinzhel2@uci.edu>
2025-03-14 11:02:29 +0000

    4

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -55,5 +55,5 @@ void print_path(const vector<int> &v, int total){
     for(size_t i = 0; i < v.size(); ++i){
         cout << v[i] << " ";
     }
-    cout << endl << total <<endl;
+    cout << endl <<"Total cost is "<< total <<endl;
 }
