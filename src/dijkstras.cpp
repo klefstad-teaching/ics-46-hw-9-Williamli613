1852bda22b0cb7619a4f31a18d066b85df5392b5
Williamli613 <jinzhel2@uci.edu>
2025-03-14 11:17:27 +0000

    7

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -20,7 +20,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     distances[source] = 0;
     previous[source] = UNDEFINED;
     priority_queue<pair<int, int>> minHeap;
-    minHeap.push({source,0});
+    minHeap.push({0, source});
     while(!minHeap.empty()){
         int u = minHeap.top().second;
         minHeap.pop();
@@ -32,7 +32,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             if(!visited[v] && distances[u] + weight < distances[v]){
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v,distances[v]});
+                minHeap.push({distances[v], v});
             }
         }
     }
